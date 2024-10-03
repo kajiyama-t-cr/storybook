81eff1d624162deb1051bc5ff750fe17147b0e59
takuya_kajiyama <kajiyama.t@creema.co.jp>
2024-10-03 11:59:16 +0900

    add storycap

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,5 @@
 import type { Preview } from "@storybook/react";
+import { withScreenshot } from "storycap";
 
 const preview: Preview = {
   parameters: {
@@ -9,6 +10,7 @@ const preview: Preview = {
       },
     },
   },
+	decorators: [withScreenshot()], // <--- just added `withScreenshot()` to `decorators`
 };
 
 export default preview;
